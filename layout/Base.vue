17ca21a8cbe8995885b3ff6eca2b67183ad8c2e1
valendion <valendion874@gmail.com>
2024-03-05 11:06:00 +0800

    initial nuxt for portfolio website

---
+++ layout/Base.vue
@@ -0,0 +1,11 @@
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+import { NuxtLayout } from "#build/components";
+</script>
+
+<style scoped></style>
